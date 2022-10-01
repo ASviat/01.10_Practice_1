c8a4373285ba6824777f353aabe1c4caaf48249a
Sviatoslav <ecrprague.asi@gmail.com>
2022-10-01 21:51:01 +0300

    Task28 Done

---
+++ Task28/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int Sum(int n1)
+{
+    int counter=1;
+    int fin=1;
+    while(counter<=n1)
+    {
+        fin=fin*counter;
+        counter++;
+    }
+    return fin;
+}
+int sum=Sum(Math.Abs(num));
+Console.WriteLine(sum);
