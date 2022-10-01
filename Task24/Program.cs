1588e75c846f3b61b9e02b5393b160fb759f5d6e
Sviatoslav <ecrprague.asi@gmail.com>
2022-10-01 14:45:09 +0300

    Task24 Done

---
+++ Task24/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int Sum(int n1)
+{
+int sum=0;
+int first=1;
+while (first<=n1)
+    {
+    sum=sum+first;
+    first++;
+    }
+Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
+return sum;
+}
+
+int sum=Sum(num);
