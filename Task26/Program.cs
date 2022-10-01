869a6f5616e4caced2f1843cde4c994cf725569a
Sviatoslav <ecrprague.asi@gmail.com>
2022-10-01 21:09:29 +0300

    Task26 Done

---
+++ Task26/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int NumQuant(int n1)
+{
+    int count=1;
+    while(n1/10!=0)
+    {
+        n1=n1/10;
+        count++;
+    }
+    return count;
+}
+int numQuant=NumQuant(num);
+Console.WriteLine($"{numQuant}");
